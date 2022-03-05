a6da61c159837a01c7bf6108ee354834926103fa
mohammad <moaziz.me@outlook.com>
2022-03-05 11:26:54 +1100

    it is a test workshop4

---
+++ workshop4.py
@@ -0,0 +1,15 @@
+# my_name = "Mohammad Aziz"
+# my_age = 35
+# my_height = 74
+# my_weight = 180
+# my_eyes = "brown"
+# my_teeth = "white"
+# my_hair = "Brown"
+#
+# print(f"""Hi, My name is {my_name}, I am {my_age} years old,
+# about {my_height} tall and {my_weight}kgs,
+# my eyes are {my_eyes}, my teeth are {my_teeth}
+# and my hair is {my_hair}""")
+
+age = input("How old are you? ")
+print(f"you age: {age}*3!")
